c1f00bb976d19c924ad4704d7021ec67b3fb31cb
bence-balazs <bestbence@gmail.com>
2023-11-20 15:56:44 +0100

    initial commit

---
+++ test_instert.py
@@ -0,0 +1,22 @@
+import database
+import os
+import sys
+
+def main():
+
+    try:
+        database.insertToDb("cpu_temp", 25111113)
+    except:
+        print("Error: wrong insert!")
+        database.closeDb
+        sys.exit(1)
+
+    try:
+        database.queryDb("cpua_temp")
+    except:
+        print("queryDb: something went wrong")
+        database.closeDb()
+        sys.exit(1)
+
+    database.closeDb()
+main()
