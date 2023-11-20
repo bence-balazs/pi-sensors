34a7171bbba00b65d7bd8f66f17fe7e5c325d861
bence-balazs <bestbence@gmail.com>
2023-11-20 15:56:36 +0100

    initial commit

---
+++ database.py
@@ -0,0 +1,25 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+conn = psycopg2.connect(
+    database=os.environ.get("pg.database"),
+    host=os.environ.get("pg.host"),
+    user=os.environ.get("pg.user"),
+    password=os.environ.get("pg.password"),
+    port=os.environ.get("pg.port"),
+)
+cursor = conn.cursor()
+
+def closeDb():
+    conn.close()
+
+def insertToDb(table, value):
+    cursor.execute(f"INSERT into {table} (temp) VALUES ({value});")
+    conn.commit()
+
+def queryDb(table):
+    cursor.execute(f"SELECT * from {table}")
+    print(cursor.fetchall())
