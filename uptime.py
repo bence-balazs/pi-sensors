712d64b7d77cc697e253c74955890be6c76ba9c7
bence-balazs <bestbence@gmail.com>
2023-11-20 18:31:42 +0100

    chore: uptime datatype

--- uptime.py
+++ uptime.py
@@ -5,14 +5,15 @@ import os
 
 def main():
     uptime = os.popen("awk '{print $1}' /proc/uptime").read()
-    print(uptime)
+    uptime = float(uptime)
+    uptime = int(uptime)
 
-    # try:
-    #     database.insertToDb("uptime", uptime)
-    # except:
-    #     database.closeDb
-    #     logger.logErrors("uptime: FAILED to insert in to the database")
-    #     sys.exit(1)
+    try:
+        database.insertToDb("uptime", uptime)
+    except:
+        database.closeDb
+        logger.logErrors("uptime: FAILED to insert in to the database")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
